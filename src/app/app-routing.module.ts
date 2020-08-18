43dfd049229a2589d1d25bfc75fe703e03d5c6d0
Kevin Caseiras <kcaseiras@gmail.com>
2020-08-18 11:26:35 -0400

    Basic routing working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CalendarsComponent} from './calendars/calendars.component';
+import {AgendasComponent} from './agendas/agendas.component';
+import {BillsComponent} from './bills/bills.component';
+import {LawsComponent} from './laws/laws.component';
+import {TranscriptsComponent} from './transcripts/transcripts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calendars', component: CalendarsComponent},
+  {path: 'agendas', component: AgendasComponent},
+  {path: 'bills', component: BillsComponent},
+  {path: 'laws', component: LawsComponent},
+  {path: 'transcripts', component: TranscriptsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillsComponent } from './bills/bills.component';
+import { TranscriptsComponent } from './transcripts/transcripts.component';
+import { LawsComponent } from './laws/laws.component';
+import { CalendarsComponent } from './calendars/calendars.component';
+import { AgendasComponent } from './agendas/agendas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent,
+    TranscriptsComponent,
+    LawsComponent,
+    CalendarsComponent,
+    AgendasComponent
   ],
   imports: [
     BrowserModule,
